61b80e2aee5f54fc6a01ab8eb4729644f3a447ff
JuanSeedtag <juangonzalez@seedtag.com>
2023-01-13 07:24:33 +0100

    c

---
+++ src/creative.js
@@ -0,0 +1,21 @@
+import './creative.css';
+
+
+
+function Creative() {
+  return (
+        <>
+        <div className="creative-container">
+          <div className="creative">
+            <div className="block bg"></div>
+            <div className="block logo"></div>
+            <div className="block copy"></div>
+            <div className="block cta"></div>
+
+          </div>
+        </div>
+        </>
+  );
+}
+
+export default Creative;
